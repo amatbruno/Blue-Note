84bd812a6674203da9edb4de8ea9dd76c12b7c2b
Bruno <brunoamagar@campus.monlau.com>
2024-04-23 16:42:59 +0200

    forms update

--- src/components/layout/admin/positions/Draggable.jsx
+++ src/components/layout/admin/positions/Draggable.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 export default function Draggable(props) {
   return (
-    <article draggable='true' className='border rounded-xl cursor-pointer hover:border-black transition-all w-[120px] py-10 px-2'>
+    <article draggable='true' className='border rounded-xl cursor-pointer 
+    hover:border-black transition-all w-[120px] py-10 px-2'>
       <h1 className='text-center text-xl font-bold'>{props.voiceName}</h1>
       <p className='text-center'>{props.voiceColor}</p>
     </article>
